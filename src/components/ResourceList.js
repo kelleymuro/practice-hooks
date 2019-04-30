32c6d2d3a0f9e09ecc873c07aa1d9803c9edea06
Kelley Muro <kelleymuro@gmail.com>
2019-04-29 20:50:36 -0700

    useResources.js is so reusable

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import axios from 'axios';
+import useResources from './useResources';
 
-class ResourceList extends React.Component {
-   state = { resources: [] };
-
-   async componentDidMount() {
-     const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
-
-     this.setState( { resources: response.data });
-   }
-
-   render() {
-      return <div> {this.state.resources.length} </div>
-   }
+    const ResourceList = ( {resource} ) => {
+       const resources = useResources(resource);
+    
+   
+   return (
+      <ul> {resources.map( record => <li key={record.id}> {record.title} </li>)} </ul> 
+   );   
 }
 
 export default ResourceList;
